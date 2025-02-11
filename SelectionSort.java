701bf2c2a6534c179a887a38399c2edeebb257de
marianacastanedar <167562546+marianacastanedar@users.noreply.github.com>
2025-02-11 11:57:21 -0600

    Selection sort ya completo

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,3 +1,37 @@
-public class SelectionSort {
+import java.util.Random;
 
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int[] arr = new int[1500]; 
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt(15000); 
+        }
+
+        long startTime = System.nanoTime();
+        selectionSort(arr);
+        long endTime = System.nanoTime();
+
+        long duration = endTime - startTime;
+
+        System.out.println("Tiempo de ejecución de Selection Sort: " + duration + " nanosegundos");
+    }
 }
